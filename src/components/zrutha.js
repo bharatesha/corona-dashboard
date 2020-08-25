b2a43fbc756a0be79f5dacc65d9ebe8d535dec69
Bharatesha M <bharatesha.m@gmail.com>
2020-08-25 23:46:47 +0300

    added zrutha page - fixed play button

--- src/components/zrutha.js
+++ src/components/zrutha.js
@@ -3,14 +3,34 @@ import i18n from "../i18n";
 import PlayCircleFilledWhiteTwoToneIcon from "@material-ui/icons/PlayCircleFilledWhiteTwoTone";
 
 function Zrutha() {
+
+ const handlePlay = (event) => {
+   //document.getElementById('zruthvideo').play();
+  // ('#zruthvideo').play();
+
+   let b = document.getElementById("vidButton");
+
+   var v = document.getElementById('zruthvideo');
+     if (v.paused) {
+      v.play();
+       b.textContent ="Pause";
+    } else {
+      v.pause();
+       b.textContent ="Play";
+    }
+ }
+
   return (
     <div className="pageCenter" >
-
-       <video  oncontextmenu="return false;" controls>
-           <source oncontextmenu="return false;" src="https://drive.google.com/uc?export=view&id=17MoPO-RFTKkNrFwwkqijxBGMtNkcbr2c" type='video/mp4'>
+    <div>
+     <button id="vidButton" onClick={handlePlay}  style={{ cursor: "pointer", marginTop: "40px", width: "100%", marginBottom: "5px", padding: "10px" }} > Play </button>
+     </div>
+    <div>
+       <video id="zruthvideo" >
+           <source src="https://drive.google.com/uc?export=view&id=11oIoeBYrFjOXc4rdefsYkeFBGishtA19" type='video/mp4'>
            </source>
        </video>
-
+    </div>
     </div>
   );
 }
